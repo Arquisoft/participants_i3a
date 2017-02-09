ecc7c6d5b55c10adb91f4221488affe95a24c01a
Pablo García Miranda <uo245530@uniovi.es>
2017-02-09 09:40:29 +0100

    RESTController finished and working properly.

--- src/main/java/es/uniovi/asw/controller/RESTController.java
+++ src/main/java/es/uniovi/asw/controller/RESTController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import es.uniovi.asw.domain.User;
 import es.uniovi.asw.dto.UserChangePassword;
@@ -16,7 +20,7 @@ import es.uniovi.asw.dto.UserLogin;
 import es.uniovi.asw.service.UserService;
 
 @RestController
-public class APIController {
+public class RESTController {
 
 	@Autowired
 	UserService userService;
@@ -26,7 +30,10 @@ public class APIController {
 		return userService.findAll();
 	}
 
-	@RequestMapping("/user")
+	@RequestMapping(value = "/user", 
+			method = RequestMethod.POST,
+			consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+    		produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserDto> user(@RequestBody UserLogin userLogin) {
 		User user = userService.findByLogin(userLogin.getLogin());
 
@@ -37,20 +44,29 @@ public class APIController {
 
 		UserDto userDto = UserDto.transform(user);
 
-		return new ResponseEntity<>(userDto, HttpStatus.OK);
+		return new ResponseEntity<UserDto>(userDto, HttpStatus.OK);
 	}
 
-	@RequestMapping("/user/changePassword")
-	public ResponseEntity<User> updatePassword(@RequestBody UserChangePassword userChangePassword) {
+	@RequestMapping(value = "/user/password",
+			method = RequestMethod.POST,
+			consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+    		produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserDto> updatePassword(@RequestBody UserChangePassword userChangePassword) {
 		User user = userService.findByLogin(userChangePassword.getLogin());
-
+		
+		//If password does not match the old password of the user, error
+		if (!user.getPassword().equals(userChangePassword.getPassword())) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 		//Passwords can not be equal
 		if (!user.getPassword().equals(userChangePassword.getPassword()))
 			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		user.setPassword(userChangePassword.getNewPassword());
 		userService.save(user);
 
-		return new ResponseEntity<>(user, HttpStatus.OK);
+		UserDto userDto = UserDto.transform(user);
+
+		return new ResponseEntity<UserDto>(userDto, HttpStatus.OK);
 	}
 
 }
