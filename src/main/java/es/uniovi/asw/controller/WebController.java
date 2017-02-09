0ae7e2d6eb432c492e5656cf40b36d950a164e3e
Pablo García Miranda <uo245530@uniovi.es>
2017-02-09 06:26:07 +0100

    WebController working properly

--- src/main/java/es/uniovi/asw/controller/WebController.java
+++ src/main/java/es/uniovi/asw/controller/WebController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import es.uniovi.asw.domain.User;
 import es.uniovi.asw.dto.UserChangePassword;
@@ -20,7 +21,7 @@ import es.uniovi.asw.dto.UserLogin;
 import es.uniovi.asw.service.UserService;
 
 @Controller
-public class MainController {
+public class WebController {
 
 	@Autowired
 	UserService userService;
@@ -41,63 +42,69 @@ public class MainController {
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(Model model) {
-		model.addAttribute("user", new UserLogin());
-		return "login";
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
+		
+		ModelAndView model = new ModelAndView("login");
+		model.addObject("user", new UserLogin());
+		return model;
 	}
 
 	@RequestMapping(value = "/info", method = RequestMethod.POST)
-	public String loginInfo(@ModelAttribute UserLogin userLogin, Model model) {
+	public ModelAndView loginInfo(@ModelAttribute("user") UserLogin userLogin, HttpServletRequest request, HttpServletResponse response) {
 
 		User user = userService.findByLogin(userLogin.getLogin());
-		
+		ModelAndView model= null;
+
 		//If the user is not in the db
 		if (user == null) {
-			return "error";
+			model = new ModelAndView("error");
 		}
 		//If the password is not correct
 		if (!user.getPassword().equals(userLogin.getPassword())) {
-			return "error";
+			model = new ModelAndView("error");
 		}
-
+		model = new ModelAndView("info");
 		UserDto userDto = UserDto.transform(user);
-		model.addAttribute("user", user);
+		model.addObject("userDto", userDto);
 
-		return "info";
+		return model;
 	}
 	
 	@RequestMapping(path = "/changePassword/{id}", method = RequestMethod.GET)
-	public String changePassword(@PathVariable long id, Model model) {
+	public ModelAndView changePassword(@PathVariable long id, HttpServletRequest request, HttpServletResponse response) {
 		
 		User user = userService.findById(id);
 		UserChangePassword userUpdated = new UserChangePassword();
-		userUpdated.setLogin(user.getLogin());
+		userUpdated.setId(user.getId());
 		
-		model.addAttribute("userUpdated", userUpdated);
+		ModelAndView model = new ModelAndView("changePassword");
+		model.addObject("user", userUpdated);
 		
-		return "changePassword";
+		return model;
 	}
 
-	@RequestMapping(path = "/changedPassword", method = RequestMethod.POST)
-	public String changePasswordPost(@ModelAttribute UserChangePassword userUpdated, Model model) {
-		User user = userService.findByLogin(userUpdated.getLogin());
+	@RequestMapping(path = "/changePassword/{id}", method = RequestMethod.POST)
+	public ModelAndView changePasswordPost(@PathVariable("id") long id, @ModelAttribute("user") UserChangePassword userUpdated, HttpServletRequest request, HttpServletResponse response) {
+		User user = userService.findById(id);
 		
+		ModelAndView model = null;
 		//If password does not match the old password of the user, error
 		if (!user.getPassword().equals(userUpdated.getPassword())) {
-			return "error";
+			model = new ModelAndView("error");
 		}
 		//If old password and new passwords are the same, error
 		if (userUpdated.getNewPassword().equals(userUpdated.getPassword())) {
-			return "error";
+			model = new ModelAndView("error");
 		}
 		//If it´s correct, set the newPassword to the user and save it in the db
 		user.setPassword(userUpdated.getNewPassword());
 		userService.save(user);
 
 		UserDto userDto = UserDto.transform(user);
-		model.addAttribute("user", userDto);
+		model = new ModelAndView("info");
+		model.addObject("userDto", userDto);
 
-		return "info";
+		return model;
 	}
 
 
