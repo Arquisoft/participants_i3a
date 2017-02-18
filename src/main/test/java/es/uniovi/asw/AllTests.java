6a6682590ac67060a580e67839187fdaab8c8546
ffreijefuente@gmail.com <damperfreije@hotmail.com>
2017-02-18 16:21:07 +0100

    Fixes on broken tests (folders werent on the build path of the project)

--- src/main/test/java/es/uniovi/asw/AllTests.java
+++ src/main/test/java/es/uniovi/asw/AllTests.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import es.uniovi.asw.dto.*;
-import es.uniovi.asw.service.*;
-import es.uniovi.asw.util.*;
-import es.uniovi.asw.domain.*;
+import es.uniovi.asw.domain.UserTest;
+import es.uniovi.asw.dto.UserDtoTest;
+import es.uniovi.asw.dto.UserLoginTest;
+import es.uniovi.asw.service.UserServiceTest;
+import es.uniovi.asw.util.AgeCalculatorTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ UserTest.class, UserServiceTest.class, UserDtoTest.class, UserLoginTest.class,
--- src/main/test/java/es/uniovi/asw/DateConversor.java
+++ src/main/test/java/es/uniovi/asw/DateConversor.java
@@ -1,5 +1,4 @@
 package es.uniovi.asw;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
