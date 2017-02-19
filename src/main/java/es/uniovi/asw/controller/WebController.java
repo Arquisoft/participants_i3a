882e8c9e71257a01dc1dfcb45ede39730f6eb01f
pablogarciamiranda <uo245530@uniovi.es>
2017-02-19 14:15:13 +0100

    Fix in navigation when changing password
    After changing the password the user can do it again without any problems

--- src/main/java/es/uniovi/asw/controller/WebController.java
+++ src/main/java/es/uniovi/asw/controller/WebController.java
@@ -36,62 +36,65 @@ public class WebController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
-		
+
 		ModelAndView model = new ModelAndView("login");
 		model.addObject("user", new UserLogin());
 		return model;
 	}
 
 	@RequestMapping(value = "/info", method = RequestMethod.POST)
-	public ModelAndView loginInfo(@ModelAttribute("user") UserLogin userLogin, HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView loginInfo(@ModelAttribute("user") UserLogin userLogin, HttpServletRequest request,
+	        HttpServletResponse response) {
 
 		User user = userService.findByLogin(userLogin.getLogin());
-		ModelAndView model= null;
+		ModelAndView model = null;
 
 		//If the user is not in the db
 		if (user == null) {
 			model = new ModelAndView("error");
 			model.addObject("errorMessage", "That username is not in our database");
 		} else if (!user.getPassword().equals(userLogin.getPassword())) {
-		//If the password is not correct
+			//If the password is not correct
 			model = new ModelAndView("error");
 			model.addObject("errorMessage", "Password is not correct");
-		} else{
+		} else {
 			model = new ModelAndView("info");
 			UserDto userDto = UserDto.transform(user);
-			model.addObject("userDto", userDto);	
+			model.addObject("userDto", userDto);
 		}
 		return model;
 	}
-	
-	@RequestMapping(path = "/changePassword/{id}", method = RequestMethod.GET)
-	public ModelAndView changePassword(@PathVariable long id, HttpServletRequest request, HttpServletResponse response) {
-		
+
+	@RequestMapping(value = "/changePassword/{id}", method = RequestMethod.GET)
+	public ModelAndView changePassword(@PathVariable long id, HttpServletRequest request,
+	        HttpServletResponse response) {
+
 		User user = userService.findById(id);
 		UserChangePassword userUpdated = new UserChangePassword();
 		userUpdated.setId(user.getId());
-		
+
 		ModelAndView model = new ModelAndView("changePassword");
 		model.addObject("user", userUpdated);
-		
+
 		return model;
 	}
 
-	@RequestMapping(path = "/changePassword/{id}", method = RequestMethod.POST)
-	public ModelAndView changePasswordPost(@PathVariable("id") long id, @ModelAttribute("user") UserChangePassword userUpdated, HttpServletRequest request, HttpServletResponse response) {
-		User user = userService.findById(id);
-		
+	@RequestMapping(value = "/changePassword", method = RequestMethod.POST)
+	public ModelAndView changePasswordPost(@ModelAttribute("user") UserChangePassword userUpdated,
+	        HttpServletRequest request, HttpServletResponse response) {
+		User user = userService.findById(userUpdated.getId());
+
 		ModelAndView model = null;
 		//If password does not match the old password of the user, error
 		if (!user.getPassword().equals(userUpdated.getPassword())) {
 			model = new ModelAndView("error");
 			model.addObject("errorMessage", "Password is not correct");
-		} else
+		}
 		//If old password and new passwords are the same, error
-		if (userUpdated.getNewPassword().equals(userUpdated.getPassword())) {
+		else if (userUpdated.getNewPassword().equals(userUpdated.getPassword())) {
 			model = new ModelAndView("error");
 			model.addObject("errorMessage", "New password is equal to old password");
-		} else{
+		} else {
 			//If it´s correct, set the newPassword to the user and save it in the db
 			user.setPassword(userUpdated.getNewPassword());
 			userService.save(user);
@@ -103,6 +106,5 @@ public class WebController {
 
 		return model;
 	}
-
 
 }
